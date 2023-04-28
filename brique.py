47b22c3157201fbf066f2a06134dc18ea605e6a2
Boubou40 <131524261+Boubou40@users.noreply.github.com>
2023-04-28 23:21:34 +0200

    Update brique.py

--- brique.py
+++ brique.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 import random
 from balle import Balle
 
+
 # Couleurs
 BLANC = (255, 255, 255)
 NOIR = (0, 0, 0)
@@ -23,31 +24,33 @@ class Brique:
         self.x = x
         self.y = y
         self.couleur = couleur
-        self.visible = True
+        self.rect = pygame.Rect(self.x, self.y, LARGEUR_BRIQUE, HAUTEUR_BRIQUE)
+        self.bonus = 'None'
 
     def dessine(self, surface):
-        pygame.draw.rect(surface, self.couleur, (self.x, self.y, LARGEUR_BRIQUE, HAUTEUR_BRIQUE))
+        pygame.draw.rect(surface, self.couleur, self.rect)
 
     def est_en_collision_avec(self, balle):
         #collision verticale
         if balle.x  > self.x and balle.x < self.x + LARGEUR_BRIQUE:
             if self.y < (balle.y + RAYON_BALLE) < (self.y + HAUTEUR_BRIQUE) or (self.y + HAUTEUR_BRIQUE) > (balle.y - RAYON_BALLE) > self.y :
                 balle.vy = -balle.vy
-                return True
+                return (True,self.bonus)
         #collision horizontale
         if balle.y  > self.y and balle.y < self.y + HAUTEUR_BRIQUE:
             if self.x < (balle.x + RAYON_BALLE) < (self.x + LARGEUR_BRIQUE) or (self.x + LARGEUR_BRIQUE) > (balle.x - RAYON_BALLE) > self.x :
                 balle.vx = -balle.vx
-                return True
+                return (True,self.bonus)
 
-        return False
+        return (False,'')
 
 class Brique_x3(Brique):
     def __init__(self, x, y, couleur):
         super().__init__(x, y, couleur)
+        self.bonus = 'x3'
 
     def dessine(self,surface):
-        pygame.draw.rect(surface, self.couleur, (self.x, self.y, LARGEUR_BRIQUE, HAUTEUR_BRIQUE))
+        pygame.draw.rect(surface, self.couleur, self.rect)
 
         # Choisir la police, la taille et la couleur du texte
         font = pygame.font.Font(None, 24)
@@ -62,15 +65,4 @@ class Brique_x3(Brique):
         # Dessiner le texte sur la surface
         surface.blit(texte, text_rect)
 
-    def activer_bonus(self):
-        global liste_balles
-        vx, vy = 0.3, 0.5
-        bonus_balle_1 = Balle(self.x + LARGEUR_BRIQUE / 2, self.y + HAUTEUR_BRIQUE, vx, vy)
-        bonus_balle_2 = Balle(self.x + LARGEUR_BRIQUE / 2, self.y + HAUTEUR_BRIQUE, vx, -vy)
-        liste_balles.extend([bonus_balle_1, bonus_balle_2])
 
-    def est_en_collision_avec(self, balle):
-        collision = super().est_en_collision_avec(balle)
-        if collision:
-            self.activer_bonus()
-        return collision
