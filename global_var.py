ea6ea5b894e22257502b28688c57bfb557a855ef
Gabriel4765 <131445070+Gabriel4765@users.noreply.github.com>
2023-05-10 15:24:29 +0200

    Add files via upload

---
+++ global_var.py
@@ -0,0 +1,12 @@
+# Couleurs
+BLANC = (255, 255, 255)
+NOIR = (0, 0, 0)
+BLEU = (0, 0, 255)
+ROUGE = (255, 0, 0)
+VERT = (0, 255, 0)
+JAUNE = (255, 255, 0)
+
+# Taille de la fenêtre
+LARGEUR_FENETRE = 500
+HAUTEUR_FENETRE = 500
+
