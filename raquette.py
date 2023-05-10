e8d3092cba28827456e99bec17dafcc66e4b926c
Boubou40 <131524261+Boubou40@users.noreply.github.com>
2023-05-10 15:23:21 +0200

    Update raquette.py

--- raquette.py
+++ raquette.py
@@ -2,34 +2,32 @@ import pygame
 from pygame.locals import *
 import random
 
-# Couleurs
-BLANC = (255, 255, 255)
-NOIR = (0, 0, 0)
-BLEU = (0, 0, 255)
+
+
 ROUGE = (255, 0, 0)
-VERT = (0, 255, 0)
-JAUNE = (255, 255, 0)
+
 
 # Taille de la fenêtre
 LARGEUR_FENETRE = 500
 HAUTEUR_FENETRE = 500
 
 
-# Taille de la raquette
-LARGEUR_RAQUETTE = 80
-HAUTEUR_RAQUETTE = 10
+
 
 # Classe pour la raquette
 class Raquette:
-    def __init__(self, x, y):
+    def __init__(self, x, y,Lr=80,Hr=10):
         self.x = x
         self.y = y
+        self.Lr = Lr
+        self.Hr = Hr
 
     def dessine(self, surface):
-        pygame.draw.rect(surface, ROUGE, (self.x, self.y, LARGEUR_RAQUETTE, HAUTEUR_RAQUETTE))
+        pygame.draw.rect(surface, ROUGE, (self.x, self.y,self.Lr,self.Hr ))
 
     def deplace(self, direction):
         if direction == "gauche" and self.x > 0:
-            self.x -= 0.60
-        elif direction == "droite" and self.x < LARGEUR_FENETRE - LARGEUR_RAQUETTE:
-            self.x += 0.60
+            self.x -= 1
+        elif direction == "droite" and self.x < LARGEUR_FENETRE - self.Lr:
+            self.x += 1
+
